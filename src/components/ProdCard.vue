0b1b8e9b4dca9bc6aa7a01998e2120a9e399f356
Yuriy Bakutin <yuriybakutin@gmail.com>
2021-10-24 23:56:27 +0300

    Added ProdCard hover animation

--- src/components/ProdCard.vue
+++ src/components/ProdCard.vue
@@ -1,15 +1,17 @@
 <template>
-  <article class="prod-card flex flex-column justify-start p3">
+  <article class="prod-card p3">
     <div class="fit flex justify-between mb3 mt1">
-      <div class="dim h3 ml1">3 этаж</div>
-      <h2 class="h3 my0 mr1">1 комната <span class="dim mx2">-</span> 22,94м²</h2>
+      <div class="dim h3 ml1 bold">3 этаж</div>
+      <h2 class="h3 my0 mr1 bold">1 комната <span class="dim mx2">-</span> 22,94м²</h2>
     </div>
     <div class="image-block relative">
-      <div class="number">№256</div>
+      <div class="number z1">№256</div>
       <div class="plan"></div>
     </div>
     <div class="price">2 729 860р.</div>
     <div class="cost-per-meter dim">119 000 р. за м²</div>
+    <button
+      class="checked h1 bold pointer detailize">Подробнее</button>
   </article>
 </template>
 <style>
@@ -19,6 +21,11 @@
   background: #fff;
   box-shadow: 0px 5px 20px rgba(86, 86, 86, 0.05);
   border-radius: 10px;
+  overflow: hidden;
+  transition: box-shadow .2s ease-in-out;
+}
+.prod-card:hover {
+  box-shadow: 0px 5px 20px rgba(86, 86, 86, 0.25);
 }
 .dim {
   opacity: 0.5;
@@ -26,10 +33,13 @@
 .image-block {
   width: 250px;
   height: 250px;
-  background: #FFFFFF;
   border: 1px solid #EBEBEB;
   box-sizing: border-box;
   border-radius: 5px;
+  transition: height .2s ease-in-out;
+}
+.prod-card:hover .image-block {
+  height: 200px;
 }
 .number {
   position: absolute;
@@ -52,7 +62,11 @@
   width: 230px;
   height: 188px;
   background: url("../img/bb8d/9eeb90d9c7aeeed41fb41c0b5e383013.png");
-  background-position: center center
+  background-position: center center;
+  transition: transform .2s ease-in-out;
+}
+.prod-card:hover .plan {
+  transform: translate3d(0px, -22px, 0px) scale(0.8);
 }
 .price {
   font-weight: bold;
@@ -69,5 +83,13 @@
   margin-top: 4px;
   margin-right: 5px;
   text-align: right;
+}
+.detailize {
+width: 250px;
+height: 40px;
+background: #70D24E;
+border-radius: 2px 2px 5px 5px !important;
+text-transform: uppercase;
+margin-top: 17px;
 }
 </style>
